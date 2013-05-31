f5fdf92aa8f054ff8b6a329909ea73ab8535baa7
Dan Barrett <dan@theblahman.net>
2013-05-31 16:14:18 +1000

    Added a categories graph

--- sabnzbd.php
+++ sabnzbd.php
@@ -5,4 +5,40 @@ $server		= $_GET['server'];
 $port		= $_GET['port'];
 $apikey		= $_GET['apikey'];
 
-$baseSabServer = $protocol . '://' . $server . ':' . $port . '/';
+$baseSabServer = $protocol . '://' . $server . ':' . $port . '/sabnzbd/';
+
+$graph = $_GET['graph'];
+
+$ch = curl_init();
+curl_setopt ( $ch , CURLOPT_HEADER , 0 );
+curl_setopt ( $ch , CURLOPT_RETURNTRANSFER , true );
+
+echo '<pre>';
+
+switch ( $graph ) {
+	case 'categories' :
+	
+		curl_setopt ( $ch , CURLOPT_URL , $baseSabServer . 'api?apikey=' . $apikey . '&mode=queue&output=json' );
+		
+		$queue = json_decode ( curl_exec ( $ch ) , true );
+		
+		$totalJobs = count ( $queue['jobs'] );
+		
+		foreach ( $queue['queue']['slots'] as $job ) {
+			$cats[] = $job['cat'];
+		}
+		
+		$finalCats = array_count_values ( $cats );
+		
+		//$cats = array_count_values ( $queue['slots'] );
+		
+		var_dump($finalCats);
+		
+		//var_dump($queue);
+	
+	break;
+}
+
+header ( 'content-type: application/json' );
+
+echo json_encode ( $finalArray );
