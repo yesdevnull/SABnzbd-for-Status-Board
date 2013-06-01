5f3e5e532dfeeedf4574990f12cecd0778e27506
Dan Barrett <dan@theblahman.net>
2013-06-01 23:16:47 +1000

    Added comments to describe what this script does

--- sabnzbd_db.php
+++ sabnzbd_db.php
@@ -19,8 +19,10 @@ function formatSizeUnits ( $bytes , $rounding = 0 ) {
 	return $bytes;
 }
 
+// Create our history database using the native SQLite 3 driver
 new SQLite3 ( 'sabnzbd_history.db' );
 
+// Now to use PDO, I'd rather use PDO than the SQLite3 driver for the rest of the script
 $db = new PDO ( 'sqlite:sabnzbd_history.db' );
 
 $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
@@ -30,7 +32,7 @@ $createHistoryTable = 'CREATE TABLE IF NOT EXISTS stats (
 						time INTEGER NOT NULL , 
 						download_speed REAL NOT NULL ,
 						speedlimit REAL NOT NULL ,
-						size_left TEXT NOT NULL,
+						size_left TEXT NOT NULL ,
 						total_downloads INTEGER NOT NULL
 					)';
 
@@ -46,21 +48,24 @@ curl_setopt ( $ch , CURLOPT_HEADER , 0 );
 curl_setopt ( $ch , CURLOPT_RETURNTRANSFER , true );
 curl_setopt ( $ch , CURLOPT_URL , $sabQueueURI );
 
+// Execute the curl command and convert it to an associative array using json_decode
 $queue = json_decode ( curl_exec ( $ch ) , true );
 
 if ( curl_errno ( $ch ) == 7 ) {
 	// SABnzbd+ server is down
-	exit;
+	die ( 'Error: SABnzbd+ Server Inaccessible' . "\n" );
 }
 
 curl_close ( $ch );
 
+// Build up our data vars from the SABnzbd+ Queue API
 $currentTime = time();
 $currentSpeed = (string) formatSizeUnits ( $queue['queue']['kbpersec'] * 1024 );
 $sizeLeft = (string) $queue['queue']['sizeleft'];
 $speedlimit = (string) $queue['queue']['speedlimit'];
 $totalDownloads = (int) count ( $queue['queue']['slots'] );
 
+// Prepare and Bind!
 $stmt = $db->prepare( 'INSERT INTO stats ( time, download_speed, speedlimit, size_left, total_downloads ) VALUES ( :time, :download_speed, :speedlimit, :size_left, :total_downloads )' );
 
 $stmt->bindParam( ':time' , $currentTime );
@@ -70,14 +75,16 @@ $stmt->bindParam( ':size_left' , $sizeLeft );
 $stmt->bindParam( ':total_downloads' , $totalDownloads );
 
 try {
+	// Execute and be verbose with what we've inserted (for logging purposes)
 	$stmt->execute();
+	
 	echo 'Data Insert Successful!' . "\n";
+	echo 'Current Time: ' . date ( 'H:i e' , $currentTime ) . "\n";
+	echo 'Current Speed: ' . $currentSpeed . "\n";
+	echo 'Size Left: ' . $sizeLeft . "\n";
+	echo 'Speed Limit: ' . $speedlimit . "\n";
+	echo 'Total Downloads: ' . $totalDownloads . "\n";
 } catch ( PDOException $e ) {
+	// SQLite error - ruh roh
 	echo 'Error: ' . $e->getMessage(). ' (' . $e->getCode() . ')';
 }
-
-echo 'Current Time: ' . date ( 'H:i e' , $currentTime ) . "\n";
-echo 'Current Speed: ' . $currentSpeed . "\n";
-echo 'Size Left: ' . $sizeLeft . "\n";
-echo 'Speed Limit: ' . $speedlimit . "\n";
-echo 'Total Downloads: ' . $totalDownloads . "\n";
